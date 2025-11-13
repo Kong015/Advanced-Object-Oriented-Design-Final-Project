c645396c89cdfeaed5c27b5d73d260b10348f604
Kong015 <kongxiong012@gmail.com>
2025-11-13 14:19:44 -0600

    Update main.cpp

--- Advanced Object-Oriented Design Final Project/main.cpp
+++ Advanced Object-Oriented Design Final Project/main.cpp
@@ -3,6 +3,29 @@ using namespace std;
 
 int main()
 {
+	int userinput;
+	while (userinput != 4)
+	{
+		if (userinput == 1)
+		{
 
+		}
+		else if (userinput == 2)
+		{
+
+		}
+		else if (userinput == 3)
+		{
+
+		}
+		else if (userinput == 4)
+		{
+
+		}
+		else
+		{
+			cout << "ERROR: Input a Number 1-4." << endl;
+		}
+	}
 	return 0;
 }
