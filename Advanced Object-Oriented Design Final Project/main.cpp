139ead5e1fc9f63b03e32271e4551994df5741f4
Kong015 <kongxiong012@gmail.com>
2025-11-13 15:02:00 -0600

    Person Class

--- Advanced Object-Oriented Design Final Project/main.cpp
+++ Advanced Object-Oriented Design Final Project/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void bankingPrompt();
+
 int main()
 {
-	int userinput;
+	int userinput = 0;
 	while (userinput != 4)
 	{
+		bankingPrompt();
+		cin >> userinput;
 		if (userinput == 1)
 		{
 
@@ -28,4 +32,12 @@ int main()
 		}
 	}
 	return 0;
+}
+
+void bankingPrompt()
+{
+	cout << "1. User Login" << endl;
+	cout << "2. Create Account" << endl;
+	cout << "3. Manager Login" << endl;
+	cout << "4. Exit" << endl;
 }
