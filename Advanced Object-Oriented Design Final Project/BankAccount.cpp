58e5d0a1d836de23cc3460925f5b22a353bb8485
brycekrause <brycekrause@icloud.com>
2025-11-19 19:56:38 -0600

    File I/O in BankAccount class

--- Advanced Object-Oriented Design Final Project/BankAccount.cpp
+++ Advanced Object-Oriented Design Final Project/BankAccount.cpp
@@ -1,23 +1,45 @@
 #include "BankAccount.h"
+#include <fstream>
+#include <iostream>
+#include <string>
 
 BankAccount::BankAccount(double b, int id) {
     std::cout << "Constructor" << std::endl;
+    balance = b;
+    this->id = id;
 }
 
 void BankAccount::Withdraw(double v) {
-    std::cout << "Old balance: " << balance << std::endl;
-    balance -= v;
-    std::cout << "New balance: " << balance << std::endl;
-    std::cout << "Withdraw of " << v << "successful!" << std::endl;
+    std::ofstream outFile("user_ " + std::to_string(id) + ".txt", std::ios::app);
+    if (outFile.is_open()) {
+        outFile << "Withdraw of " << v << " successful!" << std::endl;
+        outFile.close();
+    } else {
+        std::cerr << "Unable to open file for writing." << std::endl;
+    }
+    
 }
 
 void BankAccount::Deposit(double v) {
-    std::cout << "Old balance: " << balance << std::endl;
-    balance += v;
-    std::cout << "New balance: " << balance << std::endl;
-    std::cout << "Deposit of " << v << "successful!" << std::endl;
+    std::ofstream outFile("user_ " + std::to_string(id) + ".txt", std::ios::app);
+    if (outFile.is_open()) {
+        outFile << "Deposit of " << v << " successful!" << std::endl;
+        balance += v;
+        outFile.close();
+    } else {
+        std::cerr << "Unable to open file for writing." << std::endl;
+    }
 }
 
 void BankAccount::PrintAccountSummary() {
-    std::cout << "Balance: " << balance << std::endl;
+    std::ifstream inFile("user_ " + std::to_string(id) + ".txt", std::ios::app);
+    if (inFile.is_open()) {
+        std::string line;
+        while (std::getline(inFile, line)) {
+            std::cout << line << std::endl;
+        }
+        inFile.close();
+    } else {
+        std::cerr << "Unable to open file for writing." << std::endl;
+    }
 }
