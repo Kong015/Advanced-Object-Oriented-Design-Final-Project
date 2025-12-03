8c7b357aa553119e33aede4e1da895ec63e8a13e
Kong015 <130696368+Kong015@users.noreply.github.com>
2025-12-03 07:51:07 -0600

    Update UserAccount.h

--- Advanced Object-Oriented Design Final Project/Header/UserAccount.h
+++ Advanced Object-Oriented Design Final Project/Header/UserAccount.h
@@ -2,37 +2,42 @@
 #define USERACCOUNT_H
 #include "User.h"
 #include "BankAccount.h"
+#include <fstream>
+#include <string>
+#include <iostream>
 
-class UserAccount : public BankAccount
+class UserAccount
 {
-	private:
-		static unsigned int numOfAccounts; // keeps track of the amount of accounts created
-		static unsigned int accountID; // keeps track of the account id (increase by 1 each account created)
-		unsigned int accountNumber;
-		string accountType;
-		double balance;
-		User user;
+private:
+	static unsigned int numOfAccounts; // keeps track of the amount of accounts created
+	static unsigned int accountID; // keeps track of the account id (increase by 1 each account created)
+	unsigned int accountNumber;
+	string accountType;
+	BankAccount account;
+	User user;
 
-	public:
-		// Constructors and Destructors 
-		UserAccount(string accountType, double balance, User user);
-		~UserAccount();
+public:
+	// Constructors and Destructors 
+	UserAccount();
+	UserAccount(string accountType, double balance, User user);
+	~UserAccount();
 
-		// Program Functions
-		void createAccount() const;
-		void deleteAccount() const;
-		void loadAccount() const;
-		void login() const;
-		void print() const;
+	// Program Functions
+	void createAccount();
+	void deleteAccount() const; 
+	void loadAccount() const;
+	bool login(const string& username, const string& password);
+	void print() const;
 
-		// Getters and setters
-		static unsigned int getNumofAccounts();
-		static int getAccountID();
-		unsigned int getAccountNumber() const;
-		string getAccountType() const;
-		double getBalance() const;
-		void setAccountType(string accountType);
-		void setBalance(double balance);
+	// Getters and setters
+	static unsigned int getNumofAccounts();
+	static int getAccountID();
+	unsigned int getAccountNumber() const;
+	string getAccountType() const;
+	double getBalance() const;
+	void setAccountType(string accountType);
+	void setBalance(double balance);
 };
 
 #endif 
+
